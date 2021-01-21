4b2c570f1fe32f69dc4b67ccfd803f0c40e36c57
Arvid-pku <xjyin2019@163.com>
2021-01-21 06:47:20 +0000

    ged finetune

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # done and todo
 
 为了在自己服务器运行，安装tensorflow1.15版本
+修改run_classifier.py 进finetune
+主要是修改文件处理部分:
+    找到main、添加任务、添加对应processor 类, processor中包含了：文件路径、label种类、读取方法等，照猫画虎
+其中的processor就是task name
--- myfinetune.sh
+++ myfinetune.sh
@@ -1,11 +1,11 @@
-export BERT_BASE_DIR=/path/to/bert/uncased_L-12_H-768_A-12
-export GLUE_DIR=/path/to/glue
-
-python run_classifier.py \
-  --task_name=MRPC \
+export BERT_BASE_DIR=bert-chinese
+export DATA_DIR=geddata/subset
+export OUTDIR=bert-chinese/finetune
+CUDA_VISIBLE_DEVICES=2 python run_classifier.py \
+  --task_name=GED \
   --do_train=true \
   --do_eval=true \
-  --data_dir=$GLUE_DIR/MRPC \
+  --data_dir=$DATA_DIR \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
@@ -13,4 +13,4 @@ python run_classifier.py \
   --train_batch_size=32 \
   --learning_rate=2e-5 \
   --num_train_epochs=3.0 \
-  --output_dir=/tmp/mrpc_output/
+  --output_dir=$OUTDIR
